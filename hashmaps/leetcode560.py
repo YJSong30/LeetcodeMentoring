a243a7de0f2d6a99263c2783a06e7e2f498b16ad
youngsong1230@gmail.com <youngsong1230@gmail.com>
2025-07-24 17:58:05 -0700

    leetcode560

--- hashmaps/leetcode560.py
+++ hashmaps/leetcode560.py
@@ -44,10 +44,112 @@ class Solution:
     """
 
     def subarraySum_brute_force(self, nums: List[int], k: int) -> int:
-        pass
+        matches = 0
+        for i in range(len(nums)):
+            current_sum = 0
+            for j in range(i, len(nums)):
+                current_sum += nums[j]
+                if current_sum == k:
+                    matches += 1
+        return matches
+
+    """
+    [1,2,3]
+
+    current_sum = 0
+    i = 1
+    j = 1
+
+    Iter 1 (i = 0)
+    sum = 1
+    sum = 3 (1 match)
+    sum = 6
+
+    Iter 2 (i = 1)
+    sum = 2
+    sum = 5
+
+    Iter 2 (i = 2)
+    sum = 3  (1 match)
+
+    Total = 2 matches
+    
+    """
+
+        
 
     def subarraySum_hashmap(self, nums: List[int], k: int) -> int:
-        pass
+        seen = { 0: 1 }
+        matches = 0
+        current_prefix_sum = 0
+            
+        for num in nums:
+            current_prefix_sum += num
+            difference = current_prefix_sum - k
+            if difference in seen:
+                matches += seen[difference]
+
+            seen[current_prefix_sum] = seen.get(current_prefix_sum, 0) + 1
+
+        return matches
+
+
+'''
+current_prefix_sum - previous_prefix_sum = k
+
+1) iterate through array
+2) keep track of the current prefix_sum
+3) at each step ask "have i seen a previous prefix sum that if i subtract it from my current prefix_sum, gives me k" 
+    - current_prefix_sum - previous_prefix_sum = k
+    - prevous_prefix_sum = current_prefix_sum - k
+    - we were go through array -> complement = target - num -> have we seen this complement is in the hashmap -> two sum
+    - hashmap = { previous_prefix_sum : count }
+
+
+0   1  2  3  4  5    
+1, -1, 1, 1, 1, 1
+
+seen = { 0: 1 }
+
+
+iter 1: 
+current_prefix_sum = 1
+difference = current_prefix_sum (1) - k (3) = -2
+no -> update seen -> { 0:1, 1:1 }
+
+iter 2:
+current_prefix_sum = 0
+difference = 0 - 3 = -3
+have we seen this in our seen hashmap? -> no
+update seen -> {0:2, 1:1}
+
+iter 3:
+current_prefix_sum = 1
+difference = 1 - 3 = -2
+is this in our hashmap -> no
+update seen -> {0:2, 1:2}
+
+iter 4:
+current_prefix_sum = 2
+difference = 2 - 3 = -1
+in hashmap? -> no
+update seen -> {0:2, 1:2, 2:1}
+
+iter 5:
+current_prefix_sum = 3
+difference = 3 - 3 = 0
+is 0 in our hashmap? 
+yes -> increment count by seen[0] = 2
+update seen -> {0:3, 1:2, 2:1, 3:1}
+
+iter 6:
+current_prefix_sum = 4
+difference = 4 - 3 = 1
+have we seen 1 in our hashmap? -> yes
+increment count by seen[1] = 2 -> 2 + 2 = 4
+update 4
+
+'''
 
 if __name__ == "__main__":
     solver = Solution()
---
+++ hashmaps/prefix_sum_notes.py
@@ -0,0 +1,84 @@
+
+'''
+        0 1 2 3 4 5 6 
+nums = [1,2,3,4,5,6,7] -> [1, 3, 6, 10, 15, 21, 28]
+n = len(nums)
+
+sum(1,3) = 2 + 3 + 4 = 9
+
+def find_subarray_sum(nums, i, j): # o(n)
+  current_sum = 0
+  for k in range(i, j+1):
+    current_sum += nums[k]
+  return current_sum
+
+number of queries = q
+
+number of  queries = q
+find_subarray_sum(1, 5)
+find_subarray_sum(100, 120)
+find_subarray_sum(45, 50)
+find_subarray_sum(1, 5)
+find_subarray_sum(1, 5)
+
+
+time complexity: o(n) * o(q) = o(n * q)
+              0  1  2   3   4  5   6
+prefix_sum = [1, 3, 6, 10, 15, 21, 28]
+
+sum(i,j)
+sum(1,3) => prefix_sum[j] - prefix_sum[i-1] = 10 - 1 = 9 . t.c = o(1)
+time complexity: o(n + q)
+
+creating prefix_sum array:
+
+method 1) in-place (saves memory)
+
+def create_prefix_sum(arr):
+  for i in range(1, len(arr)):
+    arr[i] += arr[i-1]
+  return arr
+        i     j
+        0 1 2 3 4 5 6
+nums = [1,3,6,10,15,21,28]
+
+arr[1] = arr[1] + arr[0] = 3
+arr[2] = arr[2] + arr[1] = 6
+...
+
+space complexity: o(1)
+
+def find_prefix_sum(prefix_arr, i, j):
+  if i > 0:
+    return prefix_arr[j] - prefix_arr[i-1]
+  else:
+    return prefix_arr[j]
+
+method 2) creating a whole new array (adds memory, solves base case) -> padded prefix_sum
+
+prefix_arr[j+1] - prefix_arr[i] 
+
+prefix_sum = [0, 1, 3, 6, 10, 15, 21, 28]
+
+space complexity: o(n)
+prefix_sum = [0, 1, 3, 6, 10, 0, 0, 0]
+
+def padded_prefix_sum(arr): 
+  prefix_sum = [0] * (len(arr) + 1))
+  for i in range(1, len(arr)):
+    prefix_sum[i] = prefix_sum[i-1] + arr[i-1]
+  return prefix_sum
+
+nums = [1,2,3,4,5,6,7]
+prefix_sum[0] = ---
+prefix_sum[1] = prefix_sum[1-1] + arr[1-1] = 0 + 1 = 1
+prefix_sum[2] = prefix_sum[2-1] + arr[2-1] = 1 + 2 = 3
+prefix_sum[3] = prefix_sum[3-1] + arr[3-1] = 3 + 3 = 6
+...
+  
+sum(1,3) = prefix_sum[4] - prefix_sum[1] = 10 - 1 = 9
+
+
+
+'''
+
